93af27fe06a86901ada50bcd0227a3f68f2662b7
Gregor <gregorweichbrodt@gmail.com>
2017-09-07 03:17:09 +0200

    - JavaScript: changed wikipedia url to parse article content - two different url schemes now allowed - rebrush front end: Bootstrap 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -9,167 +9,123 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var changed = require('gulp-changed');
 var livereload = require('gulp-livereload');
+var uncss = require('gulp-uncss');
 
 var paths = {
-    src: 'src',
-    dist: 'dist',
-    npm: 'node_modules',
+  src: 'src',
+  dist: 'dist',
+  npm: 'node_modules',
 };
 
 function errorHandler(error) {
-    notify({
-        title: 'Gulp Task Error',
-        message: 'Check the console.'
-    }).write(error);
+  notify({
+    title: 'Gulp Task Error',
+    message: 'Check the console.'
+  }).write(error);
 
-    console.log(error.toString());
+  console.log(error.toString());
 
-    this.emit('end');
+  this.emit('end');
 }
 
 // JSHint task
 gulp.task('lint', function() {
-    gulp.src(paths.src + '/js/*.js')
-        .pipe(jshint())
-        // You can look into pretty reporters as well, but that's another story
-        .pipe(jshint.reporter('jshint-stylish'));
+  gulp.src(paths.src + '/js/*.js')
+    .pipe(jshint())
+    // You can look into pretty reporters as well, but that's another story
+    .pipe(jshint.reporter('jshint-stylish'));
 });
-
-
-
-
-
-/**
- * 	Views
- */
 
 gulp.task('views', [], function() {
 
-    // php html
-	gulp.src(paths.src + "/*.{html,php}")
-		.pipe(gulp.dest(paths.dist))
-		.pipe(livereload());
+  // php html
+  gulp.src(paths.src + "/*.{html,php}")
+    .pipe(gulp.dest(paths.dist))
+    .pipe(livereload());
 
 });
-
-
-/*
-    JavaScript
- */
 
 gulp.task('javascript', [], function() {
 
-    gulp.src([
+  gulp.src([
 
-            // Vendor
-            paths.npm + '/jquery/dist/jquery.js',
-            paths.npm + '/clipboard/dist/clipboard.js',
+      // Vendor
+      paths.npm + '/jquery/dist/jquery.js',
+      paths.npm + '/clipboard/dist/clipboard.js',
 
-            // scripts
-            // paths.src + '/js/wikis.js',
-            paths.src + '/js/scripts.js',
+      // scripts
+      // paths.src + '/js/wikis.js',
+      paths.src + '/js/scripts.js',
 
-        ], {
-            base: paths.src
-        })
-        .pipe(plumber({ errorHandler: errorHandler }))
-        //.pipe(changed(paths.dist + '/js'))
-        .pipe(concat('scripts.js'))
-        .pipe(plumber({ errorHandler: errorHandler }))
-        .pipe(uglify())
-        .pipe(plumber({ errorHandler: errorHandler }))
-        .pipe(gulp.dest(paths.dist))
-        .pipe(livereload());
+    ], {
+      base: paths.src
+    })
+    .pipe(plumber({ errorHandler: errorHandler }))
+    //.pipe(changed(paths.dist + '/js'))
+    .pipe(concat('scripts.js'))
+    .pipe(plumber({ errorHandler: errorHandler }))
+    .pipe(uglify())
+    .pipe(plumber({ errorHandler: errorHandler }))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(livereload());
 });
-
-
-
-/*
-    Sass
- */
 
 gulp.task('sass', function() {
 
-    gulp.src(paths.src + '/sass/style.scss')
-        // .pipe(sourcemaps.init())
-        .pipe(sass({
-                outputStyle: 'compressed',
-                includePaths: [paths.src + '/sass']
-            })
-            .on('error', errorHandler))
-        // .pipe(plumber({errorHandler: errorHandler}))
-        // .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.dist));
+  gulp.src(paths.src + '/sass/style.scss')
+    .pipe(sass({
+        outputStyle: 'compressed',
+        includePaths: [paths.src + '/sass']
+      })
+      .on('error', errorHandler))
+    .pipe(uncss({
+        html: [paths.src + '/index.html'],
+        ignore: [/\#output.+/, /\.btn\-.+/, /h\d/, /\.[mp][trbl]\-\d/, /\.alert.*/]
+    }))
+    .pipe(gulp.dest(paths.dist));
 
 
 });
 
-
-
-/*
-    Images
- */
-
-
- // Clean Images
- gulp.task('clean:images', [], function() {
-     return del([
-         paths.dist + '/img/**/*.{jpg,png,svg,gif,webp,ico}'
-         ], {force: true});
- });
+// Clean Images
+gulp.task('clean:images', [], function() {
+  return del([
+    paths.dist + '/img/**/*.{jpg,png,svg,gif,webp,ico}'
+  ], { force: true });
+});
 
 
 gulp.task('images', [], function() {
-
-    gulp.src(paths.src + '/img/**/*.{jpg,png,svg,gif,webp,ico}', {
-            base: paths.src
-        })
-        .pipe(gulp.dest(paths.dist))
-        .pipe(livereload());
+  gulp.src(paths.src + '/img/**/*.{jpg,png,svg,gif,webp,ico}', {
+      base: paths.src
+    })
+    .pipe(gulp.dest(paths.dist))
+    .pipe(livereload());
 
 });
 
 
-
 /**
- *
- * 	Watch
- *
+ *  Watch
  */
-
 gulp.task('watch', function() {
 
-	livereload.listen();
+  livereload.listen();
 
+  gulp.watch([
+    paths.src + "/*.{html,php}",
+  ], ['views']);
 
-    /*
-        views
-     */
+  gulp.watch([
+    paths.src + '/js/*.js',
+  ], ['javascript']);
 
-    gulp.watch([
-        paths.src + "/*.{html,php}",
-    ], ['views']);
-
-
-    /*
-        app
-     */
-
-    gulp.watch([
-        paths.src + '/js/*.js',
-    ], ['javascript']);
-
-    // Sass
-    gulp.watch(paths.src + '/sass/**/*.scss', ['sass']);
-
-    // Images
-    gulp.watch(paths.src + '/img/**/*', ['images']);
-
-
+  gulp.watch(paths.src + '/sass/**/*.scss', ['sass']);
+  gulp.watch(paths.src + '/img/**/*', ['images']);
 
 });
 
 gulp.task('default', [
-    'watch',
-    'views', 'javascript', 'sass', 'images'
+  'watch',
+  'views', 'javascript', 'sass', 'images'
 ]);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,244 +1,202 @@
-(function ($, root, undefined) {
+var debug = false;
+if (!debug) {
+  console.debug = function() {};
+}
 
-	var debug = false;
-	if (!debug) {
-		console.log = function() {};
-	}
+function parseCell(cellItem, options) {
 
+  // remove invisible elements in cells
+  var every_el = cellItem.find('*');
+  for (var i = 0; i < every_el.length; i++) {
+    if ($(every_el[i]).css('display') == 'none') {
+      $(every_el[i]).remove();
+    }
+  }
 
-	function parseCell(cell_item, options) {
+  var line = cellItem.text();
 
-		// remove invisible elements in cells
-		var every_el = cell_item.find('*');
-		for (var i = 0; i < every_el.length; i++) {
-			if ($(every_el[i]).css('display') == 'none' ) {
-				$(every_el[i]).remove();
-			}
-		}
+  if (options.trim === true) {
+    line = line.trim();
+  }
 
-		var line = cell_item.text();
+  if (options.remove_n === true) {
+    line = line.replace(/\r?\n|\r/g, ' ');
+  }
 
-		if (options.trim === true) {
-			line = line.trim();
-		}
+  // escape double quotes in line
+  if (/\"/.test(line)) {
+    line = line.replace(/\"/g, '""');
+  }
 
-		if (options.remove_n === true) {
-			line = line.replace(/\r?\n|\r/g, ' ');
-		}
+  // put line in double quotes
+  // if line break, comma or quote found in line
+  if (/\r|\n|\"|,/.test(line)) {
+    line = '"' + line + '"';
+  }
 
-		// escape double quotes in line
-		if (/\"/.test(line)) {
-			line = line.replace(/\"/g, '""');
-		}
+  return line;
+}
 
-		// put line in double quotes
-		// if line break, comma or quote found in line
-		if (/\r|\n|\"|,/.test(line)) {
-			line = '"' + line + '"';
-		}
+function clearOutput() {
+  $('#output').removeClass('output-generated').find('> .result').empty();
+}
 
-		return line;
-	}
+$(document).ready(function() {
 
+  var form = $('form');
 
-	function clearOutput() {
-		$('#output').removeClass('output-generated').find('> .result').empty();
-	}
+  /*
+  	Parse-Button
+   */
 
+  $('#button-parse').click(function(event) {
+    event.preventDefault();
+    
+    var urlVal = form.find('input[name="url"]').val().trim();
+    var title = null;
+    var langSlug = null;
 
-	$(document).ready(function() {
-		
-		var form = $('form');
+    // Parse Url
+    // Accept schemes:
+    // 1. https://en.wikipedia.org/wiki/Lists_of_earthquakes
+    // 2. https://fr.wikipedia.org/w/index.php?title=Wikip%C3%A9dia:Rapports/Nombre_de_pages_par_namespace&action=view
+    var urlMatch = urlVal.match(/^https?\:\/{2}(\w+)\.wikipedia\.org\/(wiki\/|w\/index\.php)(.+)$/);
+    if (urlMatch != null) {
 
-		/*
-			Parse-Button
-		 */
-		
-		var lastUsedUrl = '';
-		$('#button-parse').click(function(event) {
-			event.preventDefault();
+	    // lang slug
+	    langSlug = urlMatch[1];
 
+	    // title
+	    if (/^wiki\/$/.test(urlMatch[2])) {
+	    	// scheme 1
+	    	var matchTitle = urlMatch[3].match(/^([^&\#]+)/)
+	    	if (matchTitle != null) {
+	    		title = matchTitle[1];
+	    	}
+	    } else if (/^w\/index\.php$/.test(urlMatch[2])) {
+	    	// scheme 2
+	    	var matchTitle = urlMatch[3].match(/title\=([^&\#]+)/)
+	    	if (matchTitle != null) {
+	    		title = matchTitle[1];
+	    	}
+	    }
 
-			/*
-				Parse Url
-			 */
-			
-			var wiki_url = form.find('input[name="url"]').val();			
+    }
 
-			if ( wiki_url == lastUsedUrl && $('#output').hasClass('output-generated') ) {
-				return;
-			}else {
-				lastUsedUrl = wiki_url;
-			}
+    if (urlMatch == null || title == null || langSlug == null) {
+    	alert('Error parsing Wikipedia url. Please enter a valid Wikipedia url (e. g. https://en.wikipedia.org/wiki/List_of_airports)');
+    	return;
+    }
 
-			var match = wiki_url.match(/^(https?\:\/\/)?(\w+)\.wikipedia\.org\/wiki\/(.+)$/);
-			if (match === null) {
-				alert('Error parsing Wikipedia url (e. g. https://en.wikipedia.org/wiki/List_of_airports)');
-				return;
-			}
-
-			var title = match[3];
-			var domain_slug = match[2];
-			var table_selector = form.find('input[name="table-selector"]').val();
-			var url = 'http://' + domain_slug + '.wikipedia.org/api/rest_v1/page/html/' + decodeURIComponent(title);
-			var options = {
-				'trim': $('#opt_trim')[0].checked,
-				'remove_n': $('#opt_remove_n')[0].checked,
-			};
-
-
-			// clear output
-			clearOutput();
-
-			console.log('Title: ' + title);
-			console.log('Domain Slug: ' + domain_slug);
-			console.log('Table selector: ' + table_selector);
-			console.log('URL: ' + url);
-
-			$.ajax({
-				url: url, 
-				type: 'GET',
-				dataType: 'text'
-			}) 
-			.done(function(html) {
-				
-				/*
-					https://en.wikipedia.org/wiki/Lists_of_earthquakes
-					https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita
-
-				 */
-
-				console.log('Request completed');
-
-				var tempDom = $('<output>').append($.parseHTML(html));
-				var tables = tempDom.find(table_selector);
-
-				if ( tables.length <= 0) {
-					alert('Error: could not find any tables on page ' + wiki_url);
-					return;
-				}
-
-				tables.each(function(idx, table_el) { 
-
-					console.log('Parsing table ' + idx);
-
-					var csv = '';
-					$(table_el).find('tr').each(function(index, tr_el) {
-
-						var el = $(tr_el);
-						var parsed_cell = '';
-
-						if ( el.find('> th, > td').length ) {
-							var row = el.find('th, td');
+    var queryUrl = 'https://' + langSlug + '.wikipedia.org/w/index.php?title=' + title + '&action=render'
+    var tableSelector = form.find('input[name="table-selector"]').val();
+    var options = {
+      'trim': $('#opt_trim')[0].checked,
+      'remove_n': $('#opt_remove_n')[0].checked,
+    };
 
 
-							var row_len = row.length;
-							for (var i = 0; i < row_len; i++) {
-								parsed_cell = parseCell($(row[i]), options);
+    // clear output
+    clearOutput();
 
-								var csv_line = parsed_cell;
+    console.debug('Title: ' + title);
+    console.debug('Lang Slug: ' + langSlug);
+    console.debug('Table selector: ' + tableSelector);
+    console.debug('URL: ' + queryUrl);
+    console.debug('Options', options);
 
-								if (i == (row_len - 1)) {
-									csv += csv_line + '\n';
-								}else {
-									csv += csv_line + ',';
-								}
-							}
+    $.ajax({
+        url: queryUrl,
+        type: 'GET',
+        dataType: 'text'
+      })
+      .done(function(html) {
 
-						}
+        /*
+        	Test URLs:
+        	https://en.wikipedia.org/wiki/Lists_of_earthquakes
+        	https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)_per_capita
+					https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Rapports/Nombre_de_pages_par_namespace
+					https://fr.wikipedia.org/w/index.php?title=Wikip%C3%A9dia:Rapports/Nombre_de_pages_par_namespace&action=view
+         */
 
-						/*
-						
-						create js object
+        console.debug('Request completed');
+        var removedImgs = html.replace(/<img[^>]*>/g, '');
+        var tempDom = $('<output>').append($.parseHTML(removedImgs));
+        var tables = tempDom.find(tableSelector);
 
-						if ( el.find('th').length ) { 
-							var th = el.find('th');
-							var th_len = th.length;
-							for (var i = th_len - 1; i >= 0; i--) {
-								parsed_cell = parseCell($(th[i]).text(), options);
-								header[i] = parsed_cell;
-								csv += parsed_cell + ',';
-								if (i == (th_len - 1)) {
-									csv += parsed_cell + '\n';
-								}
-							}
-						}else if ( el.find('td').length ) {
-							// console.log('found TD!');
-							var td = el.find('td');
-							var td_len = td.length;
-							new_obj = {};
-							for (var idx = td_len - 1; idx >= 0; idx--) {
-								parsed_cell = parseCell($(td[idx]).text(), options);
-								new_obj[header[idx]] = parsed_cell;
-								csv += parsed_cell + ',';
-								if (idx == (td_len - 1)) {
-									csv += parsed_cell + '\n';
-								}
-							}
-							data.push(new_obj);
-						}
+        if (tables.length <= 0) {
+          alert('Error: could not find any tables on page ' + queryUrl);
+          return;
+        }
 
-						*/
+        tables.each(function(idx, table_el) {
 
-					});
+          console.debug('Parsing table ' + idx);
 
-					// console.log('Header:'); 
-					// console.dir(header); 
-					// console.log('Data:'); 
-					// console.dir(data); 
+          var csv = '';
+          $(table_el).find('tr').each(function(index, tr_el) {
 
-					var block_id = idx+1;
-					var csv_container = '<div class="output-block">' +
-											'<h3>Table ' + block_id + '</h3>' +
-											'<p>' +
-												'<button class="copy-button" data-clipboard-target="#copytarget-' + block_id + '">Copy to clipboard</button>' +
-												'<span class="copy-msg">Copied!</span>' +
-											'</p>' +
-											'<textarea id="copytarget-' + block_id + '">'+csv+'</textarea>' +
-										'</div>';
-					$('#output').addClass('output-generated').find('> .result').append(csv_container);
+            var el = $(tr_el);
+            var parsed_cell = '';
 
-				});
+            if (el.find('> th, > td').length) {
+              var row = el.find('th, td');
 
 
-				// console.log('Table:'); 
-				// console.dir(table);
+              var row_len = row.length;
+              for (var i = 0; i < row_len; i++) {
+                parsed_cell = parseCell($(row[i]), options);
+
+                var csvLine = parsed_cell;
+
+                if (i == (row_len - 1)) {
+                  csv += csvLine + '\n';
+                } else {
+                  csv += csvLine + ',';
+                }
+              }
+
+            }
+
+          });
+
+          var blockId = idx + 1;
+          var csvContainer = '<div class="output-block mb-5">' +
+            '<h5>Table ' + blockId + '</h5>' +
+            '<textarea id="copytarget-' + blockId + '" class="form-control" rows="7">' + csv + '</textarea>' +
+            '<div class="mt-2">' +
+            '<button class="btn btn-outline-primary copy-button" data-clipboard-target="#copytarget-' + blockId + '">Copy to clipboard</button>' +
+            '<span class="copy-msg">Copied!</span>' +
+            '</div>' +
+            '</div>';
+          $('#output').addClass('output-generated').find('> .result').append(csvContainer);
+
+        });
+
+      })
+      .fail(function() {
+        console.error('Error!');
+      })
+      .always(function() {
+        // console.debug("complete");
+      });
 
 
+    return false;
+  });
 
+  $('#output h2 a').click(function(event) {
+    // clear output
+    clearOutput();
+    return false;
+  });
 
+  // init clipboard functions
+  var clipboard = new Clipboard('.copy-button');
+  clipboard.on('success', function(e) {
+    $(e.trigger).next('.copy-msg').fadeIn(200).delay(200).fadeOut(200);
+    // e.clearSelection();
+  });
 
-			})
-			.fail(function() {
-				console.error('Error!');
-			})
-			.always(function() {
-				// console.log("complete");
-			});
-			
-
-			return false;
-		});
-
-		$('#output h2 a').click(function(event) {
-			// clear output
-			clearOutput();
-			return false;
-		});
-
-		// init clipboard functions
-		var clipboard = new Clipboard('.copy-button');
-		clipboard.on('success', function(e) {
-			$(e.trigger).next('.copy-msg').fadeIn(200).delay(200).fadeOut(200);
-			// e.clearSelection();
-		});
-
-	});
-
-
-
-
-
-
-
-})(jQuery, this);
+});
